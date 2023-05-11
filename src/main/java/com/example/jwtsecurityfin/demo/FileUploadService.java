6be2d3e9789fa0654254294c5ea40e007662aedb
seungjaelee <dhssnfl258@inha.edu>
2023-05-11 16:54:11 +0900

    가계부 이미지 등록 완료. Upload Controller, Service 분리 완료

---
+++ src/main/java/com/example/jwtsecurityfin/demo/FileUploadService.java
@@ -0,0 +1,35 @@
+package com.example.jwtsecurityfin.demo;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+public class FileUploadService {
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public FileUploadService(AmazonS3Client amazonS3Client) {
+        this.amazonS3Client = amazonS3Client;
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+        String fileUrl = "https://tribillbucket.s3.ap-northeast-2.amazonaws.com/" + fileName;
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+        amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
+        return fileUrl;
+    }
+}
